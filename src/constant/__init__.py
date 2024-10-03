437c3c59b6815141c99170f7f5fe44a1d048e257
unknown <saxenakarishma48.com>
2024-10-03 22:42:35 +0530

    all modules completed

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,13 +1,15 @@
 import os
 
 
-AWS_S3_BUCKET_NAME=""
-MONGO_DATABASE_NAME=""
-MONGO_COLLECTION_NAME=""
+AWS_S3_BUCKET_NAME="wafer-fault"
+MONGO_DATABASE_NAME="mlproject"
+MONGO_COLLECTION_NAME="wafer fault"
 
 
-TARGET_COLUMN=""
-MONGO_DB_URL=""
+TARGET_COLUMN="quality"
+MONGO_DB_URL="mongodb+srv://saxenakarishma48:karishma48@cluster0.b4bln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 MODEL_FILE_NAME="model"
 MODEL_FILE_EXTENSION=".pkl"
+
+artifact_folder="artifacts"
